0fe7f4646c3d288284f1897f93fdc893c74213ec
Icaro Re Depaolini <icaro.redepaolini@oaks.cloud>
2022-05-22 17:18:10 +0200

    aggiunta prima navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'rational_thinking';
+  navbar_background = '../assets/images/navbar.jpg';
+  menuStatus: boolean = false;
+
+  openMenu() {
+    this.menuStatus = !this.menuStatus;
+  }
 }
