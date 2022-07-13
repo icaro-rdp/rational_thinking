da5d626d277629f98ddebd457f27720e7fc56a45
Icaro Re Depaolini <icaro.redepaolini@oaks.cloud>
2022-07-13 16:26:41 +0200

    spostamento cartelle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BurgerComponent } from './shared/burger/burger.component';
-import { MenuComponent } from './menu/menu.component';
-import { TopScrollerComponent } from './shared/top-scroller/top-scroller.component';
-import { ArticleComponent } from './shared/article/article.component';
-import { ArticlesDisplayerComponent } from './shared/articles-displayer/articles-displayer.component';
+import { ArticleComponent } from './home/articles-displayer/article/article.component';
+import { ArticlesDisplayerComponent } from './home/articles-displayer/articles-displayer.component';
+import { MenuComponent } from './home/menu/menu.component';
 import { ContentfulService } from './services/contentful.service';
+import { BurgerComponent } from './shared/burger/burger.component';
+import { TopScrollerComponent } from './shared/top-scroller/top-scroller.component';
 
 @NgModule({
   declarations: [
